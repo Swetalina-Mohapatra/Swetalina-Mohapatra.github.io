9b419146db6df75a5c339c088b6dbe5871bf0f4a
Swetalina Mohapatra <swetalinaa.mohapatra04@gmail.com>
2026-08-20 19:52:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function openModal(imageSrc, captionText) {
+  const modal = document.getElementById("imageModal");
+  const modalImg = document.getElementById("modalImg");
+  const caption = document.getElementById("modalCaption");
 
+  modal.style.display = "flex";
+  modalImg.src = imageSrc;
+  caption.textContent = captionText;
+}
+
+function closeModal() {
+  document.getElementById("imageModal").style.display = "none";
+}
+
+// Close modal if user presses Escape key
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    closeModal();
+  }
+});
